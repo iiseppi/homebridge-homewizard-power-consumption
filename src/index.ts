8e97cde712d8fbf828d9362024df307cd0d21c0d
iiseppi <47714239+iiseppi@users.noreply.github.com>
2024-12-14 11:36:49 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { API } from 'homebridge';
 import { HomewizardPowerConsumption } from './Platform';
 
+const FakeGatoHistoryService = require('fakegato-history');
+
 export = (api: API) => {
   api.registerPlatform('HomewizardPowerConsumption', HomewizardPowerConsumption);
 };
