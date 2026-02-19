9e02099dcb5b4764e57b269f9b123f6e5cd02a40
iiseppi <47714239+iiseppi@users.noreply.github.com>
2026-02-19 17:51:44 +0200

    Add settings for HomeWizard plugin constants
    fix

---
+++ src/settings.ts
@@ -0,0 +1,8 @@
+export const PLATFORM_NAME = 'HomeWizardPowerConsumption';
+export const PLUGIN_NAME = 'homebridge-homewizard-power-consumption';
+
+// Eve Energy Custom Characteristics UUIDs
+export const EveVoltageUUID = 'E863F10A-079E-48FF-8F27-9C2605A29F52';
+export const EveAmperageUUID = 'E863F126-079E-48FF-8F27-9C2605A29F52';
+export const EveConsumptionUUID = 'E863F10D-079E-48FF-8F27-9C2605A29F52';
+export const EveTotalConsumptionUUID = 'E863F10C-079E-48FF-8F27-9C2605A29F52';
