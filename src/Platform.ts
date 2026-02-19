62a43cdded97d1c5fa66f582ecea592593b50f86
iiseppi <47714239+iiseppi@users.noreply.github.com>
2026-02-19 17:13:26 +0200

    Update Platform.ts
    api v2

--- src/Platform.ts
+++ src/Platform.ts
@@ -1,26 +1,43 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
 import axios from 'axios';
-import { HomewizardPowerConsumptionAccessory, HomewizardDevice } from './PlatformTypes';
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 import PowerConsumption from './Accessories/PowerConsumption';
 import PowerReturn from './Accessories/PowerReturn';
 
+/**
+ * HomewizardPowerConsumption Platform
+ */
 export class HomewizardPowerConsumption implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
+
+  // Käytetään tallentamaan löydetyt lisälaitteet
   public readonly accessories: PlatformAccessory[] = [];
+  
   private heartBeatInterval: number;
-  private devices: HomewizardPowerConsumptionAccessory[] = [];
-  private device: HomewizardDevice;
+  private devices: any[] = []; // Tähän tallennetaan käynnistetyt lisälaiteoliot
+  private deviceData: any;
+  private apiPath = '/api/v1'; // Oletus, päivitetään initialise-vaiheessa
 
+  constructor(
+    public readonly log: Logger,
+    public readonly config: PlatformConfig,
+    public readonly api: API,
+  ) {
+    this.heartBeatInterval = (config.pollInterval || 10) * 1000;
 
-  constructor(public readonly log: Logger, public readonly config: PlatformConfig, public readonly api: API) {
-    this.heartBeatInterval = (config.pollInterval || 60) * 1000;
+    this.log.debug('Platform alustettu, odotetaan Homebridgen käynnistystä...');
+
     this.api.on('didFinishLaunching', () => {
       this.initialise();
     });
   }
 
+  /**
+   * Homebridge kutsuu tätä, kun se palauttaa välimuistista vanhan lisälaitteen.
+   */
   public configureAccessory(accessory: PlatformAccessory) {
+    this.log.debug('Ladataan lisälaite välimuistista:', accessory.displayName);
     this.accessories.push(accessory);
   }
 
@@ -28,132 +45,110 @@ export class HomewizardPowerConsumption implements DynamicPlatformPlugin {
     return !!this.config.ip;
   }
 
-  private async validateIp(): Promise<boolean> {
+  /**
+   * Tarkistetaan laitteen IP ja selvitetään tukeeko se API v2:sta.
+   */
+  private async discoverDevice(): Promise<boolean> {
+    const ip = this.config.ip;
     try {
-      const { data } = await axios.get(`http://${this.config.ip}/api/`, { timeout: 2000 });
-      if (data && data.product_type === 'HWE-P1') {
-        this.device = data;
+      // Kokeillaan ensin v2:sta
+      this.log.debug(`Yritetään yhdistää: ${ip} (API v2)...`);
+      const response = await axios.get(`http://${ip}/api/v2`, { timeout: 3000 });
+      this.deviceData = response.data;
+      this.apiPath = '/api/v2';
+      this.log.info(`HomeWizard laite tunnistettu! Käytetään API v2 -rajapintaa.`);
+      return true;
+    } catch (error) {
+      try {
+        // Jos v2 epäonnistuu, kokeillaan v1:stä
+        this.log.debug(`v2 ei vastannut, yritetään API v1...`);
+        const response = await axios.get(`http://${ip}/api/v1`, { timeout: 3000 });
+        this.deviceData = response.data;
+        this.apiPath = '/api/v1';
+        this.log.info(`HomeWizard laite tunnistettu! Käytetään API v1 -rajapintaa.`);
         return true;
+      } catch (err) {
+        return false;
       }
-      return false;
-    } catch(error) {
-      return false;
     }
   }
 
   private async initialise() {
     if (!this.validateConfig()) {
-      this.log.error('Configuration error. Please provide your Wi-Fi P1 meter\'s IP address');
+      this.log.error('Konfiguraatiovirhe: Anna HomeWizard-laitteen IP-osoite asetuksissa.');
       return;
     }
 
-    if (!await this.validateIp()) {
-      this.log.error('Your Wi-Fi P1 meter\'s IP address seems to be incorrect. No connection possible');
+    if (!await this.discoverDevice()) {
+      this.log.error(`Yhteys osoitteeseen ${this.config.ip} epäonnistui. Tarkista IP ja API-asetukset.`);
       return;
     }
 
-    this.setupAccessoires();
+    this.setupAccessories();
 
+    // Ensimmäinen haku heti
     await this.heartBeat();
 
+    // Säännöllinen päivitys
     setInterval(() => {
       this.heartBeat();
     }, this.heartBeatInterval);
   }
 
-  private setupAccessoires() {
+  private setupAccessories() {
+    // 1. Power Consumption (Kulutus)
+    const consumptionName = 'Power Consumption';
+    const consumptionUuid = this.api.hap.uuid.generate('homewizard-power-consumption');
+    const existingConsumption = this.accessories.find(acc => acc.UUID === consumptionUuid);
 
-    const powerConsumptionName = 'Power Consumption';
-    const powerConsumptionUuid = this.api.hap.uuid.generate('homewizard-power-consumption');
-    const powerConsumptionExistingAccessory = this.accessories.find(accessory => accessory.UUID === powerConsumptionUuid);
     if (this.config.hidePowerConsumptionDevice !== true) {
-      if (powerConsumptionExistingAccessory) {
-        this.devices.push(new PowerConsumption(this.config, this.log, this.api, powerConsumptionExistingAccessory, this.device));
+      if (existingConsumption) {
+        this.log.debug('Palautetaan olemassa oleva kulutus-sensori');
+        this.devices.push(new PowerConsumption(this.config, this.log, this.api, existingConsumption, this.deviceData));
       } else {
-        this.log.info(`${powerConsumptionName} added as accessory`);
-        const accessory = new this.api.platformAccessory(powerConsumptionName, powerConsumptionUuid);
-        this.devices.push(new PowerConsumption(this.config, this.log, this.api, accessory, this.device));
-        this.api.registerPlatformAccessories('homebridge-homewizard-power-consumption', 'HomewizardPowerConsumption', [accessory]);
-      }
-    } else {
-      if (powerConsumptionExistingAccessory) {
-        this.api.unregisterPlatformAccessories(powerConsumptionUuid, 'homebridge-homewizard-power-consumption', [powerConsumptionExistingAccessory]);
+        this.log.info('Lisätään uusi kulutus-sensori');
+        const accessory = new this.api.platformAccessory(consumptionName, consumptionUuid);
+        this.devices.push(new PowerConsumption(this.config, this.log, this.api, accessory, this.deviceData));
+        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
       }
     }
 
-    const powerReturnName = 'Power Return';
-    const powerReturnUuid = this.api.hap.uuid.generate('homewizard-power-return');
-    const powerReturnExistingAccessory = this.accessories.find(accessory => accessory.UUID === powerReturnUuid);
+    // 2. Power Return (Tuotto/Palautus)
+    const returnName = 'Power Return';
+    const returnUuid = this.api.hap.uuid.generate('homewizard-power-return');
+    const existingReturn = this.accessories.find(acc => acc.UUID === returnUuid);
+
     if (this.config.hidePowerReturnDevice !== true) {
-      if (powerReturnExistingAccessory) {
-        this.devices.push(new PowerReturn(this.config, this.log, this.api, powerReturnExistingAccessory, this.device));
+      if (existingReturn) {
+        this.log.debug('Palautetaan olemassa oleva tuotto-sensori');
+        this.devices.push(new PowerReturn(this.config, this.log, this.api, existingReturn, this.deviceData));
       } else {
-        this.log.info(`${powerReturnName} added as accessory`);
-        const accessory = new this.api.platformAccessory(powerReturnName, powerReturnUuid);
-        this.devices.push(new PowerReturn(this.config, this.log, this.api, accessory, this.device));
-        this.api.registerPlatformAccessories('homebridge-homewizard-power-consumption', 'HomewizardPowerConsumption', [accessory]);
-      }
-    } else {
-      if (powerReturnExistingAccessory) {
-        this.api.unregisterPlatformAccessories(powerReturnUuid, 'homebridge-homewizard-power-consumption', [powerReturnExistingAccessory]);
+        this.log.info('Lisätään uusi tuotto-sensori');
+        const accessory = new this.api.platformAccessory(returnName, returnUuid);
+        this.devices.push(new PowerReturn(this.config, this.log, this.api, accessory, this.deviceData));
+        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
       }
     }
   }
-  
-// Tässä HomeWizard-lisälaitteen luokka
-class HomeWizardAccessory {
-    constructor(log, config, api) {
-        this.log = log;
-        this.config = config;
-        this.api = api;
 
-        // Fakegato-historian alustaminen
-        this.loggingService = new FakeGatoHistoryService('energy', this, {
-            storage: 'fs',
-            path: './fakegatoStorage',
-            disableTimer: false
-        });
-
-        this.currentPower = 0; // Alkuarvo
-        this.totalConsumption = 0; // Alkuarvo
-
-        this.service = new this.api.hap.Service.Switch(this.config.name);
-        this.service.addCharacteristic(this.loggingService);
-
-        this.api.on('didFinishLaunching', () => {
-            this.updateEnergyData();
-        });
-    }
-
-    updateEnergyData() {
-        // Lisää energiankulutustiedot Fakegatoon
-        setInterval(() => {
-            this.currentPower = Math.random() * 1000; // Simuloitu kulutus
-            this.totalConsumption += this.currentPower / 3600000;
-
-            this.loggingService.addEntry({
-                time: Math.round(new Date().valueOf() / 1000),
-                power: this.currentPower,
-                total: this.totalConsumption
-            });
-
-            this.log(`Power: ${this.currentPower} W, Total: ${this.totalConsumption.toFixed(2)} kWh`);
-        }, 60000); // 1 min päivitys
-    }
-}
-
-  
+  /**
+   * Hakee datan laitteelta ja lähettää sen sensoreille
+   */
   private async heartBeat() {
+    const url = `http://${this.config.ip}${this.apiPath}/data`;
     try {
-      const { data } = await axios.get(`http://${this.config.ip}/api/v1/data`);
-      const consumption = data.active_power_w as number;
-      this.devices.forEach((device: HomewizardPowerConsumptionAccessory) => {
-        device.beat(consumption);
+      const { data } = await axios.get(url, { timeout: 5000 });
+      
+      // Lähetetään data kaikille rekisteröidyille laitteille
+      this.devices.forEach(device => {
+        if (typeof device.beat === 'function') {
+          device.beat(data);
+        }
       });
-      this.log.debug('heart beat', consumption);
-    } catch(error) {
-      this.log.error('Something went wrong, please double check the Wi-Fi P1 meter\'s IP address');
-      this.log.debug(error);
+
+      this.log.debug(`Datan haku onnistui (${this.apiPath}): ${data.active_power_w} W`);
+    } catch (error: any) {
+      this.log.error(`Virhe haettaessa dataa (${url}): ${error.message}`);
     }
   }
 }
